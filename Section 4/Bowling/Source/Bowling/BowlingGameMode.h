5391bbc467a828c090954133af8ae907ca3a1b39
Santiago Regueiro <santi@regueiro.es>
2016-11-10 21:36:06 +0100

    Lecture 48 - Unreal project

---
+++ Section 4/Bowling/Source/Bowling/BowlingGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BowlingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWLING_API ABowlingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
